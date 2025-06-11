24632ab1017ecd503166bedf7cc66c56dd32c63a
a24braisgp <a24braisgp@iessanclemente.net>
2025-06-11 12:09:21 +0200

    up build.sh

--- build.sh
+++ build.sh
@@ -4,10 +4,13 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+python manage.py migrate
+
+python manage.py init_site
+
 if [[$CREATE_SUPERUSER]]
 then
   python manage.py createsuperuser --no-input
+fi
 
-
-
-python manage.py migrate
--- todasXogan/restApi/management/commands/init_site.py
+++ todasXogan/restApi/management/commands/init_site.py
@@ -1,16 +1,17 @@
 from django.core.management.base import BaseCommand
 from django.contrib.sites.models import Site
-import os
 
 class Command(BaseCommand):
-    help = 'Initialize default Site object'
+    help = 'Crea el objeto Site con ID 1 para que allauth y admin funcionen correctamente'
 
-    def handle(self, *args, **options):
-        domain = os.environ.get('SITE_DOMAIN', 'restapitodasxogan.onrender.com')
-        name = os.environ.get('SITE_NAME', 'restapitodasxogan')
-        site, created = Site.objects.get_or_create(id=1, defaults={'domain': domain, 'name': name})
+    def handle(self, *args, **kwargs):
+        domain = 'restapitodasxogan.onrender.com'
+        name = 'restapitodasxogan'
+        site, created = Site.objects.get_or_create(
+            id=1, defaults={'domain': domain, 'name': name}
+        )
         if not created:
             site.domain = domain
             site.name = name
             site.save()
-        self.stdout.write(self.style.SUCCESS(f'Site initialized: {site.domain}'))
+        self.stdout.write(self.style.SUCCESS(f'Site configurado: {site.domain}'))
