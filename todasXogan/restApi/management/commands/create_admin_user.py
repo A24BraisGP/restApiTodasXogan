5341d782daa884dc4cfe48173333f7c28fc175a2
a24braisgp <a24braisgp@iessanclemente.net>
2025-06-11 13:08:54 +0200

    permission classes

--- todasXogan/restApi/management/commands/create_admin_user.py
+++ todasXogan/restApi/management/commands/create_admin_user.py
@@ -3,20 +3,17 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Crea un superusuario desde variables de entorno si no existe'
+    help = 'Crea un superusuario desde entorno o valores por defecto si no existe'
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **kwargs):
         User = get_user_model()
-        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
-        email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
-        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
-        if not username or not email or not password:
-            self.stdout.write(self.style.WARNING('Faltan variables de entorno para el superusuario'))
-            return
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'root')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@todasxogan.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123!!')
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS(f'Superusuario "{username}" creado correctamente.'))
+            self.stdout.write(self.style.SUCCESS(f'Superusuario "{username}" creado.'))
         else:
             self.stdout.write(f'Superusuario "{username}" ya existe.')
