6efaf36614aa49f22c32f6d2dff7cd12d89b9f85
a24braisgp <a24braisgp@iessanclemente.net>
2025-06-12 14:57:13 +0200

    permissions

--- todasXogan/settings.py
+++ todasXogan/settings.py
@@ -84,7 +84,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'https://todas-xogan-frontend.vercel.app',
-    'https://restapitodasxogan.onrender.com'
+    'https://restapitodasxogan.onrender.com',
     'http://localhost:5173', 
     'http://127.0.0.1:5173', 
 ]
