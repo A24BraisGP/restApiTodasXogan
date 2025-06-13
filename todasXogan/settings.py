a19f43dd22a39a669da3137158367360d42e000c
A24BraisGP <a24braisgp@iessanclemente.net>
2025-06-13 09:03:28 +0200

    cors

--- todasXogan/settings.py
+++ todasXogan/settings.py
@@ -192,5 +192,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    
+     'DEFAULT_PERMISSION_CLASSES':
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
 }
