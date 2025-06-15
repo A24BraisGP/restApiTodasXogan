7c9fda7b92cda427caaee6bab1068f3028ce5548
a24braisgp <a24braisgp@iessanclemente.net>
2025-06-15 12:49:11 +0200

    added revision jsonparser

--- todasXogan/restApi/views.py
+++ todasXogan/restApi/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics, status
 from .models import *
 from .serializers import *
-from rest_framework.parsers import MultiPartParser, FormParser 
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -203,7 +203,7 @@ class PropostaVideoxogoDetailView(generics.RetrieveUpdateDestroyAPIView):
 class PropostaVideoxogoRevisionView(generics.UpdateAPIView):
     queryset = PropostaVideoxogo.objects.all()
     serializer_class = PropostaVideoxogoSerializer
-    parser_classes= (MultiPartParser,FormParser,)
+    parser_classes= (MultiPartParser,FormParser,JSONParser,)
 
 
     def get_queryset(self):
